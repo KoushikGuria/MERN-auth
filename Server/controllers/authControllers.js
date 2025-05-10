855eff1cfb929c9c2228dbfd967dcf27dfde9541
Koushik Guria <koushikguria998@gmail.com>
2025-05-10 17:54:37 +0530

    update otp verification features

--- Server/controllers/authControllers.js
+++ Server/controllers/authControllers.js
@@ -135,3 +135,47 @@ export const sendVerifyOtp = async (req, res) => {
         return res.json({success: false, message: error.message});
     }
 }
+
+// Verify OTP and activate the account
+export const verifyEmail = async (req, res) => {
+    const {userId, otp} = req.body;
+
+    if (!userId || !otp) {
+        return res.json({success: false, message: "Missing required fields"});
+    }
+    try {
+        const user = await userModel.findById(userId);
+        if (!user) {
+            return res.json({success: false, message: "User not found"});
+        }
+
+        if (user.verifyOtp === '' || user.verifyOtp !== otp) {
+            return res.json({success: false, message: "Invalid OTP"});
+        }
+
+        if (user.verifyOtpExpireAt < Date.now()) {
+            return res.json({success: false, message: "OTP expired"});
+        }
+
+        user.isAccountVerified = true;
+        user.verifyOtp = '';
+        user.verifyOtpExpireAt = 0;
+
+        await user.save();
+
+        return res.json({success: true, message: "Account verified successfully"});
+
+    } catch (error) {
+        return res.json({success: false, message: error.message});
+    }
+}
+
+// Check if the user is authenticated
+export const isAuthenticated = async (req, res) => {
+    try {
+        return res.json({success: true, message: "User is authenticated"});
+
+    } catch (error) {
+        return res.json({success: false, message: error.message});
+    }
+}
